9d1cc603721ef5d949de831e7d236c5370d182c3
Berkay Yıldız <yildizberkay359@gmail.com>
2025-05-05 19:27:32 +0300

    Creating a Global Exception Handler in Backend

--- Presentation/ETicaretAPI.API/Program.cs
+++ Presentation/ETicaretAPI.API/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.API.Configurations.ColumnWriters;
+using ETicaretAPI.API.Extensions;
 using ETicaretAPI.Application;
 using ETicaretAPI.Application.Validators.Products;
 using ETicaretAPI.Infrastructure;
@@ -106,6 +107,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.ConfigureExceptionHandler<Program>(app.Services.GetRequiredService<ILogger<Program>>());
+
 
 app.UseStaticFiles();
 
